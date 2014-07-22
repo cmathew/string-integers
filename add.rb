7e458238346914fc573dca7d569bae590cba7d62
cmathew <chris.w.mathew@gmail.com>
2014-07-21 19:05:20 -0700

    same print effect, but with Calc class

--- add.rb
+++ add.rb
@@ -1,7 +1,14 @@
 #!/usr/bin/env ruby
+class Calculator
+	def add(num_string)
+		num_string.split(',').each {|s| puts s.strip.to_i}
+	end
+end  
+
+calc = Calculator.new
 
 while true do
 	puts "Please enter a string of numbers, comma-delineated:"
 	num_string = gets
-	num_string.split(',').each {|s| puts s.strip.to_i}
+	calc.add(num_string)
 end
