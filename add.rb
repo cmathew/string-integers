f2cc6f4cef5a263dc2040d2214b5066409ecb455
cmathew <chris.w.mathew@gmail.com>
2014-07-21 22:37:46 -0700

    Step 4

--- add.rb
+++ add.rb
@@ -1,16 +1,30 @@
 #!/usr/bin/env ruby
 class Calculator
-	def add(num_string)
+	def add(numbers)
 		total = 0
-		num_string.split(/[\n,]+/).each {|s| total += s.strip.to_i}
+		numbers.each {|s| total += s.strip.to_i}
 		puts "Total is #{total}"	
+	end
+	
+	#find delimiter in input string
+	def parse_intput(param_string)	
+		default = param_string.eql? "\n"		
+		return "," if default
+		param_string.split(/\n/)[0]
+	end
+	
+	def parse_numbers(num_string, delimiter)
+		delimiting_regex = Regexp.new("[#{delimiter}]")
+		num_string.split(delimiting_regex)
 	end
 end  
 
 calc = Calculator.new
 
 while true do
-	puts "Please enter a string of integers, comma-delineated:"
-	num_string = gets
-	calc.add(num_string)
+	puts "You can enter an optional delimiter (default will be [,])"
+	delimiter = calc.parse_intput(gets)
+	puts "Enter a [#{delimiter}] delimited string of integers"
+	num_array = calc.parse_numbers(gets, delimiter)
+	calc.add(num_array)
 end
