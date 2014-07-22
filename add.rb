245f3ad89202aa68331381c66beedd56a6f231a7
cmathew <chris.w.mathew@gmail.com>
2014-07-21 19:13:43 -0700

    Step 1
    enforce a maximum of three integers

--- add.rb
+++ add.rb
@@ -1,14 +1,17 @@
 #!/usr/bin/env ruby
 class Calculator
 	def add(num_string)
-		num_string.split(',').each {|s| puts s.strip.to_i}
+		total = 0
+		num_string.split(',').each {|s| total += s.strip.to_i}
+		puts "Total is #{total}"	
 	end
 end  
 
 calc = Calculator.new
 
 while true do
-	puts "Please enter a string of numbers, comma-delineated:"
+	puts "Please enter a string of up to three numbers, comma-delineated:"
 	num_string = gets
-	calc.add(num_string)
+	#act on first three numbers only
+	calc.add(num_string.split(',')[0..2].join(','))
 end
